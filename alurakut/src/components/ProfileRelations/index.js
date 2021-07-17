d51eba11cda0983b7de4fd8f618a58eca5bfcfb7
Andre Gomes <andgomes95@gmail.com>
2021-07-16 21:33:11 -0300

    add AnimeWrapped

--- alurakut/src/components/ProfileRelations/index.js
+++ alurakut/src/components/ProfileRelations/index.js
@@ -53,6 +53,39 @@ export const ProfileRelationsBoxWrapper = styled(Box)`
   }
 `;
 
+export const AnimeBoxWrapper = styled(Box)`
+  ul {
+    display: grid;
+    grid-gap: 8px;
+    grid-template-columns: 1fr 3fr; 
+    list-style: none;
+  }
+  img {
+    object-fit: cover;
+    background-position: center center;
+    width: 100%;
+    height: 100%;
+    position: relative;
+  }
+  ul li a {
+    display: inline-block;
+    height: 102px;
+    position: relative;
+    overflow: hidden;
+    border-radius: 8px;
+    &:after {
+      content: "";
+      display: block;
+      position: absolute;
+      top: 0;
+      right: 0;
+      left: 0;
+      bottom: 0;
+      z-indeX: 1;
+      background-image: linear-gradient(0deg,#00000073,transparent);
+    }
+  }
+`;
 
 export function ProfileSidebar(props ){
   return(
